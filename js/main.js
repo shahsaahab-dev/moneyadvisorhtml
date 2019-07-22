81ae00fc33e0e6f5383ac60228a030be49893e35
Syed Naqi Ali Shah <naqishah2997@gmail.com>
2019-07-22 03:02:59 -0700

    HTML Template

---
+++ js/main.js
@@ -0,0 +1,79 @@
+
+$('.card-owl').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:true,
+    dots:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:false,
+            loop:true,
+            dots:true,
+        }
+    }
+});
+
+
+// Testimonial Slider 
+$('.testimonials').owlCarousel({
+    loop:true,
+    margin:50,
+    nav:true,
+    dots:false,
+    navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+         320:{
+          items:1,
+            nav:false,
+        },
+        600:{
+            items:3,
+            nav:true
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:true
+        }
+    }
+});
+
+
+$('.other-debts').owlCarousel({
+    loop:true,
+    margin:35,
+    nav:true,
+    dots:false,
+    navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false,
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:true
+        }
+    }
+})
