83698d74f7978785fcb0265f6a20f524b6a64cf7
Syed Naqi Ali Shah <naqishah2997@gmail.com>
2019-07-22 04:17:39 -0700

    navbar and other changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,47 @@
+(function ($) {
+    "use strict";  
+// Commons Variables
+    var $window = $(window),
+        $body = $('body');
+    
+    // Mobile Menu
+    $('.mobile-menu-toggle').on('click', function(e) {
+        e.preventDefault();
+        $body.addClass('mobile-menu-open');
+    });
+    $('.mobile-menu-close, .offcanvas-menu-close').on('click', function(e) {
+        e.preventDefault();
+        $body.removeClass('mobile-menu-open');
+    });
+    var $offCanvasNav = $('.offcanvas-navigation'),
+        $offCanvasNavSubMenu = $offCanvasNav.find('.sub-menu');
+    /*Add Toggle Button With Off Canvas Sub Menu*/
+    $offCanvasNavSubMenu.parent().prepend('<span class="menu-expand"><i></i></span>');
+    /*Category Sub Menu Toggle*/
+    $offCanvasNav.on('click', 'li a, li .menu-expand', function(e) {
+        var $this = $(this);
+        if ( $this.parent().attr('class').match(/\b(menu-item-has-children|has-children|has-sub-menu)\b/) || ($this.attr('href') === '#' || $this.hasClass('menu-expand')) ) {
+            e.preventDefault();
+            if ($this.siblings('ul:visible').length){
+                $this.parent('li').removeClass('active').children('ul').slideUp().siblings('a').find('.menu-expand i').removeClass('zmdi-chevron-up').addClass('zmdi-chevron-down');
+                $this.parent('li').siblings('li').removeClass('active').find('ul:visible').slideUp().siblings('a').find('.menu-expand i').removeClass('zmdi-chevron-up').addClass('zmdi-chevron-down');
+            } else {
+                $this.parent('li').addClass('active').children('ul').slideDown().siblings('a').find('.menu-expand i').removeClass('zmdi-chevron-down').addClass('zmdi-chevron-up');
+                $this.parent('li').siblings('li').removeClass('active').find('ul:visible').slideUp().siblings('a').find('.menu-expand i').removeClass('zmdi-chevron-up').addClass('zmdi-chevron-down');
+            }
+        }
+    });
+    
+    // Close When Click Outside Elements
+    $body.on('click', function(e){
+        var $target = e.target;
+        
+        // Mobile Menu Close
+        if (!$($target).is('.offcanvas-mobile-menu, .mobile-menu-toggle') && !$($target).parents().is('.offcanvas-mobile-menu, .mobile-menu-toggle')) {
+            $body.removeClass('mobile-menu-open');
+        }
+    });
+    
 
 $('.card-owl').owlCarousel({
     loop:true,
@@ -81,3 +125,5 @@ $('.other-debts').owlCarousel({
         }
     }
 })
+    
+    })(jQuery);
